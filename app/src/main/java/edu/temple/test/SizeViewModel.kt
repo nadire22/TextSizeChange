a542bd2980402546e870aa18de5a1c51d1d8d58c
nadire22 <112009353+nadire22@users.noreply.github.com>
2022-10-18 11:47:43 -0400

    Create SizeViewModel.kt

---
+++ app/src/main/java/edu/temple/test/SizeViewModel.kt
@@ -0,0 +1,19 @@
+package edu.temple.test
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SizeViewModel: ViewModel() {
+
+     private val size : MutableLiveData<Float> = MutableLiveData()
+
+    fun SetSize(size:Float){
+        this.size.value = size
+    }
+
+    fun getSize(): LiveData<Float>{
+        return size
+    }
+
+}
